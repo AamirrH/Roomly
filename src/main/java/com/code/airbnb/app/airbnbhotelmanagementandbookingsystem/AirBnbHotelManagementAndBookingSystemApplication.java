b4b3f1fec8d949fcd4c5e0c98adf0450695cdf4c
AamirrH <aamirr.1704@gmail.com>
2026-02-06 17:14:16 +0530

    initialise repo

---
+++ src/main/java/com/code/airbnb/app/airbnbhotelmanagementandbookingsystem/AirBnbHotelManagementAndBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.code.airbnb.app.airbnbhotelmanagementandbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBnbHotelManagementAndBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirBnbHotelManagementAndBookingSystemApplication.class, args);
+    }
+
+}
