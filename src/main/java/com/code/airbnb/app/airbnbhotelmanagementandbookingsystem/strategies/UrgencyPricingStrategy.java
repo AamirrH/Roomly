edacb1515bd478e12bbaba7be8b5368ac450d88e
AamirrH <aamirr.1704@gmail.com>
2026-04-02 00:11:59 +0530

    add : Urgency Pricing Strategy added

--- src/main/java/com/code/airbnb/app/airbnbhotelmanagementandbookingsystem/strategies/UrgencyPricingStrategy.java
+++ src/main/java/com/code/airbnb/app/airbnbhotelmanagementandbookingsystem/strategies/UrgencyPricingStrategy.java
@@ -1,16 +1,23 @@
 package com.code.airbnb.app.airbnbhotelmanagementandbookingsystem.strategies;
 
 import com.code.airbnb.app.airbnbhotelmanagementandbookingsystem.entities.Inventory;
+import com.code.airbnb.app.airbnbhotelmanagementandbookingsystem.exceptions.BookingExpiredException;
+import com.code.airbnb.app.airbnbhotelmanagementandbookingsystem.repositories.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 
 // Raise Price if currentDate is 2-3 days before checkInDate
 @RequiredArgsConstructor
 public class UrgencyPricingStrategy implements PricingStrategy{
 
+    private static final int urgencyWindow = 4;
+    private static final double urgencyRate4Days = 1.25;
+    private static final double urgencyRateLessThan4Days = 1.5;
+    private static final double urgencyRate0Days = 2;
     private final PricingStrategy wrapped;
 
     @Override
@@ -19,7 +26,20 @@ public class UrgencyPricingStrategy implements PricingStrategy{
 
         // Get Current Date
         LocalDate today = LocalDate.now();
+        // Urgency -> 4 days before booking
+        long daysBetween = ChronoUnit.DAYS.between(today, inventory.getDate());
 
+        if(daysBetween == 0){
+            price = price.multiply(BigDecimal.valueOf(urgencyRate0Days));
+        }
+        else if(daysBetween < urgencyWindow){
+            price = price.multiply(BigDecimal.valueOf(urgencyRateLessThan4Days));
+
+        }
+        else if(daysBetween == urgencyWindow){
+            price = price.multiply(BigDecimal.valueOf(urgencyRate4Days));
+        }
+        return price;
 
     }
 }
